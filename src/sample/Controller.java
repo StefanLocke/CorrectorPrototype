9e7ec0d6fb30b64c1b8729b5f986e3f0c816147d
StefanLocke <StefanLocke@users.noreply.github.com>
2021-01-20 18:23:36 +0100

    more changes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,11 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -28,6 +31,7 @@ public class Controller {
     public ListView leftList;
     public ListView rightList;
 
+
     public void toggleTop() throws IOException {
         topPane.setVisible(!topShow);
         topButtonHidden.setVisible(topShow);
@@ -44,15 +48,13 @@ public class Controller {
     double startY ;
 
     public void dragBottom(MouseEvent event) {
-        System.out.println(event.getSceneY());
         if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
-            bottomPane.setPrefHeight(bottomPane.getScene().getHeight()-event.getSceneY());
-            bottomPane.setMaxHeight(bottomPane.getScene().getHeight()-event.getSceneY());
+            bottomPane.setPrefHeight(Math.max(0,Math.min(bottomPane.getScene().getHeight()-100,bottomPane.getScene().getHeight()-event.getSceneY())));
+
         }
         if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
             startY = event.getY();
         }
-
     }
 
 
